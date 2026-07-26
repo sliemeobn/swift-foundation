d04230bd8b6dcd8cb580ed89a01a511dce804434
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2026-07-26 22:36:17 +0200

    fix swift-collections dependency

--- Package.swift
+++ Package.swift
@@ -69,7 +69,7 @@ if let useLocalDepsEnv = Context.environment["SWIFTCI_USE_LOCAL_DEPS"], !useLoca
         [
             .package(
                 url: "https://github.com/apple/swift-collections",
-                .upToNextMinor(from: "1.4.0")),
+                from: "1.6.0"),
             .package(
                 url: "https://github.com/apple/swift-foundation-icu",
                 branch: "main"),
